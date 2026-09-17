20a4500e25f175a873f1f087eb7503ce4483ef01
wkelison guilherme <wkguilherme22@gmail.com>
2026-09-17 14:45:14 +0000

    aula 29

--- src/fap/aula 29/APIs-2.test.ts
+++ src/fap/aula 29/APIs-2.test.ts
@@ -22,3 +22,38 @@ test("metodo POST para criar um novo post", async () => {
   expect(dados.title).toBe("meu novo post");
   expect(dados.body).toBe('conteudo do meu novo post');
 });
+test('metodo PUT para ATUALIZAR um post', async() => {
+  const res = await fetch(`${base_url}/posts/1`,{
+    method:'PUT',
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+      userId: 1,
+      title: "meu novo post",
+      body: "conteudo do meu novo post",
+  }),
+   });
+    expect(res.status).toBe(200);
+
+});
+test('metodo PATCH para ATUALIZAR um post', async() => {
+  const res = await fetch(`${base_url}/posts/1`,{
+    method:'PATCH',
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+      userId: 1,
+      title: "meu titulo atualizado",
+    }),
+  });
+    expect(res.status).toBe(200);
+
+  });
+  test('metodo DELETE para ATUALIZAR um post', async() => {
+  const res = await fetch(`${base_url}/posts/1`,{
+    method:'DELETE',
+    });
+    expect(res.status).toBe(200);
+  });
